fd100ba3f96c52e3c46dc30b28bdeb400bd7df10
filosof777 <olimjonkamalov@mail.ru>
2021-11-24 20:58:21 +0500

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
-let elNavList = document.querySelector('.nav__list');
-let elHeaderBtn = document.querySelector('.header__btn');
+let elBtnLeft = document.querySelector('.btn:nth-child(1)');
+let elBtnRight = document.querySelector('.btn:nth-child(2)');
+let elList = document.getElementsByClassName('list');
+let elItem = document.getElementsByClassName('item');
+let a = 0;
 
-elHeaderBtn.addEventListener('click', () => {
-  elNavList.classList.toggle('show-menu');
-});
+function slideLeft() {
+  for (let i = 0; i < elItem.length; i++) {
+    if (elItem[i].style.left == 0) {
+      elItem[i].style.left = "0%";
+      break;
+    }
+  }
+}
 
+function slideRight() {
+  for (let i = 0; i < elItem.length; i++) {
+    if (elItem[i].style.left == 0) {
+      elItem[i].style.left = "0%";
+      break;
+    }
+  }
+}
