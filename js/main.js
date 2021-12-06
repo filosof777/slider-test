17ca723fbba67858f0d0bbe234af5f0c4b535fc7
filosof777 <olimjonkamalov@mail.ru>
2021-12-06 13:54:46 +0500

    pushing

--- js/main.js
+++ js/main.js
@@ -1,23 +1,32 @@
 let elBtnLeft = document.querySelector('.btn:nth-child(1)');
 let elBtnRight = document.querySelector('.btn:nth-child(2)');
-let elList = document.getElementsByClassName('list');
-let elItem = document.getElementsByClassName('item');
-let a = 0;
+let elList = document.querySelector('.list');
+let elItem = document.querySelectorAll('.item');
+let elImg = document.querySelector('.img');
+let a = 1;
+let count = 0;
+let value = 0;
 
-function slideLeft() {
-  for (let i = 0; i < elItem.length; i++) {
-    if (elItem[i].style.left == 0) {
-      elItem[i].style.left = "0%";
-      break;
-    }
-  }
-}
+elBtnRight.addEventListener('click', () => {
+  count++;
+  value += 100;
+  elList.style.transform = `translateX(${-value}%)`;
 
-function slideRight() {
-  for (let i = 0; i < elItem.length; i++) {
-    if (elItem[i].style.left == 0) {
-      elItem[i].style.left = "0%";
-      break;
-    }
+  if (count == 4) {
+    elList.style.transform = `translateX(0px)`;
+    count = 0;
+    value = 0;
   }
-}
+})
+
+elBtnLeft.addEventListener('click', () => {
+  if (value == 0) {
+    elList.style.transform = `translateX(400%)`;
+    count = 4;
+    value = 400;
+  }
+
+  count--;
+  value -= 100;
+  elList.style.transform = `translateX(${-value}%)`;
+})
